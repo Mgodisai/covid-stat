89b1b3e1a2503e485e03cf9c5bd17dafe89b5805
Varga Tamás <mgodisai@gmail.com>
2024-08-09 23:46:36 +0200

    noAuthGuard implemented on registration endpoint

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,17 @@ import { LoginComponent } from './auth/login/login.component';
 import { authGuard } from './auth/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { RegistrationComponent } from './auth/registration/registration.component';
+import { noauthGuard } from './auth/noauth.guard';
 
 export const routes: Routes = [
    { path: 'login', component: LoginComponent },
-   { path: 'registration', component: RegistrationComponent },
+
    { path: '', component: HomeComponent },
+   {
+      path: 'registration',
+      component: RegistrationComponent,
+      canActivate: [noauthGuard],
+   },
    {
       path: 'stats',
       canActivate: [authGuard],

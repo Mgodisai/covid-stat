efe76c89ae55bf3ebe028508c7b7e23061dfd4df
Varga Tamás <mgodisai@gmail.com>
2024-08-10 23:57:17 +0200

    implemented google map in countryComponent

--- src/app/stats/single-country-stat/country/country.component.ts
+++ src/app/stats/single-country-stat/country/country.component.ts
@@ -1,14 +1,40 @@
 import { Component, Input, signal } from '@angular/core';
 import { Country } from '../../models/country.model';
 import { NgbAccordionModule } from '@ng-bootstrap/ng-bootstrap';
+import { GoogleMapsModule, MapAdvancedMarker } from '@angular/google-maps';
 
 @Component({
    selector: 'app-country',
    standalone: true,
-   imports: [NgbAccordionModule],
+   imports: [NgbAccordionModule, GoogleMapsModule, MapAdvancedMarker],
    templateUrl: './country.component.html',
    styleUrl: './country.component.scss',
 })
 export class CountryComponent {
    @Input() countryData?: Country;
+   latlng = signal({ lat: 0, lng: 0 });
+
+   options: google.maps.MapOptions = {
+      mapId: 'MAP_ID',
+      center: this.latlng(),
+      zoom: 6,
+   };
+
+   ngOnInit(): void {
+      this.updateLatLng();
+   }
+
+   updateLatLng(): void {
+      if (this.countryData) {
+         this.latlng.set({
+            lat: parseFloat(this.countryData.lat),
+            lng: parseFloat(this.countryData.long),
+         });
+
+         this.options = {
+            ...this.options,
+            center: this.latlng(),
+         };
+      }
+   }
 }
